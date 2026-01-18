6b6782280dc46c2d420e85d745975cf925eb3cad
Waelalessa21 <waeelalessa5@gmail.com>
2026-01-18 14:35:23 -0800

    Changes to be committed: 	modified:   .metadata 	new file:   lib/core/routing/app_router.dart 	modified:   lib/core/routing/routes.dart 	modified:   lib/data_app.dart 	modified:   lib/main.dart 	new file:   lib/pages/home/ui/home_screen.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml 	new file:   web/favicon.png 	new file:   web/icons/Icon-192.png 	new file:   web/icons/Icon-512.png 	new file:   web/icons/Icon-maskable-192.png 	new file:   web/icons/Icon-maskable-512.png 	new file:   web/index.html 	new file:   web/manifest.json

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:data_app/core/routing/app_router.dart';
 import 'package:data_app/data_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const DataApp());
+  runApp(DataApp(appRouter: AppRouter()));
 }
